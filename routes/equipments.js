53e821031865035f3de2951879066e00d1437643
dvggtk <dvggtk@gmail.com>
2020-06-14 18:24:58 +1000

    настроил правильное кэширование фото

--- routes/equipments.js
+++ routes/equipments.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const etag = require("etag");
+
 const fs = require("fs");
 const path = require("path");
 
@@ -25,40 +27,57 @@ router.get("/:uuid", function (req, res, next) {
   })().catch((err) => next(err));
 });
 
-router.get("/img/:imgfile", function (req, res, next) {
-  const imgfile = req.params.imgfile;
-  const pathname = path.resolve(__dirname, "../storage/db/img/", imgfile);
+router.get(
+  "/img/:imgfile",
+  function (req, res, next) {
+    const imgfile = req.params.imgfile;
+    const filepath = path.resolve(__dirname, "../storage/db/img/", imgfile);
+
+    req.filepath = filepath;
+    next();
+  },
+  sendFile
+);
+
+router.get(
+  "/img/original/:imgfile",
+  function (req, res, next) {
+    const imgfile = req.params.imgfile;
+    const filepath = path.resolve(
+      __dirname,
+      "../storage/db/img/original",
+      imgfile
+    );
+
+    req.filepath = filepath;
+    next();
+  },
+  sendFile
+);
+
+async function sendFile(req, res, next) {
+  const filepath = req.filepath;
+
+  if (!filepath) return next(new Error("Не указан файл для отправки"));
+  const filename = filepath.match(/[\\\/]([^\\\/]+)[.][^\\\/]*$/)[1];
+
+  const etagValue = etag(filename);
 
   res.set({
-    "Cache-Control": "immutable"
+    // 1. https://engineering.fb.com/web/this-browser-tweak-saved-60-of-requests-to-facebook/
+    // 2. https://tools.ietf.org/html/rfc8246#section-2.2
+    "Cache-Control": "max-age=31536000, immutable",
+    ETag: etagValue
   });
 
-  fs.createReadStream(pathname)
+  if (req.headers["if-none-match"] === etagValue) return res.status(304).end();
+
+  fs.createReadStream(filepath)
     .on("error", (err) => {
       if (err.code === "ENOENT") return next();
       return next(err);
     })
     .pipe(res);
-});
-
-router.get("/img/original/:imgfile", function (req, res, next) {
-  const imgfile = req.params.imgfile;
-  const pathname = path.resolve(
-    __dirname,
-    "../storage/db/img/original",
-    imgfile
-  );
-
-  res.set({
-    "Cache-Control": "immutable"
-  });
-
-  fs.createReadStream(pathname)
-    .on("error", (err) => {
-      if (err.code === "ENOENT") return next();
-      return next(err);
-    })
-    .pipe(res);
-});
+}
 
 module.exports = router;
